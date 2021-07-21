4694d05cee28ca38fc894605184d1fb33e0fffaf
Ignacio Pérez <ignacio.perez-martinez@zueblin.de>
2021-07-21 12:32:23 +0200

    plot_type

--- src/buildings.py
+++ src/buildings.py
@@ -4,20 +4,26 @@ import Rhino.Geometry as rg
 
 class Buildings:
 
-    def __init__ (self, plot_polylines, street_width, building_depth):
+    def __init__ (self, plot_polylines, street_width, min_building_width, max_building_width, block_factor_min_dis):
         self.plot_polylines = plot_polylines
         self.street_width = street_width
-        self.building_depth = building_depth
+        self.min_building_width = min_building_width
+        self.max_building_width = max_building_width
+        self.block_factor_min_dis = block_factor_min_dis
 
     
     def offset_plot(self):
-        return offset_curves(self.plot_polylines, self.street_width)
+        if isinstance(self.plot_polylines):
+            offset_plots = [offset_curve(plot, self.street_width) for plot in self.plot_polylines]
+        else:
+            offset_plots = offset_curve(self.plot_polylines, self.street_width)
+        return offset_plots
 
 
     def plots_type(self):
         building_types = []
         for plot in self.plot_polylines:
-            building_types.append(define_plot_type(plot, self.building_depth))
+            building_types.append(plot_type(plot, self.building_depth, self.block_factor_min_dis))
         return building_types
 
 
--- src/utility.py
+++ src/utility.py
@@ -6,35 +6,27 @@ from itertools import combinations_with_replacement
 import ghpythonlib as gh
 
 
-def offset_curves(polycurve, offset_distance, direction = 1):
-    offLst =[]
-    for plot in polycurve:
-        try:
-            plot = rs.coercecurve(plot)
-            nurb = plot.ToNurbsCurve()
-            centroid = rg.AreaMassProperties.Compute(nurb).Centroid
-            offseted_crv = rs.OffsetCurve(nurb, centroid, offset_distance * direction, rg.Vector3d.ZAxis)
-            offseted_crv = rs.coercecurve(offseted_crv[0]).ToPolyline()
-            off_nurb = offseted_crv.ToNurbsCurve()
-            intersection_count = rg.Intersect.Intersection.CurveCurve(nurb, off_nurb, offset_distance -1, 0).Count
-            #offLst.append(intersection_count)
-            if offseted_crv.IsClosedWithinTolerance(0.1) and intersection_count == 0:
-                offLst.append(offseted_crv)
-        except:
-           pass
-    return offLst
+def offset_curve(polycurve, offset_distance, direction = 1):
+    try:
+        plot = rs.coercecurve(polycurve)
+        nurb = plot.ToNurbsCurve()
+        centroid = rg.AreaMassProperties.Compute(nurb).Centroid
+        offseted_crv = rs.OffsetCurve(nurb, centroid, offset_distance * direction, rg.Vector3d.ZAxis)
+        offseted_crv = rs.coercecurve(offseted_crv[0]).ToPolyline()
+        off_nurb = offseted_crv.ToNurbsCurve()
+        intersection_count = rg.Intersect.Intersection.CurveCurve(nurb, off_nurb, offset_distance -1, 0).Count
+        if offseted_crv.IsClosedWithinTolerance(0.1) and intersection_count == 0:
+            return offseted_crv
+    except:
+        pass
 
 
-def define_plot_type(polycurve, building_tiefe):
-    offset_1 = building_tiefe * 0.6
-    offset_2 = building_tiefe * 1.15
-    polycurve_offset_1 = gh.components.ClipperComponents.PolylineOffset([polycurve], offset_1, rg.Plane.WorldXY, 0.1, 2,2 ,0.1)
-    polycurve_offset_2 = gh.components.ClipperComponents.PolylineOffset([polycurve], offset_2, rg.Plane.WorldXY, 0.1, 2,2 ,0.1)
-    plot_type = 0
-    if polycurve_offset_2[1]:
-        plot_type = 2
-    elif polycurve_offset_1[1]:
-        plot_type = 1
-    return plot_type
+def plot_type(polycurve, min_building_width, block_factor_min_dis):
+    offset_crv = offset_curve(polycurve, block_factor_min_dis * min_building_width)
+    if offset_crv:
+        brep = rg.Brep.CreateFromLoft([polycurve, offset_crv], rg.Point3d.Unset, rg.Point3d.Unset, rg.LoftType.Straight, False)
+        return brep[0]
+    else:
+        pass
 
 
